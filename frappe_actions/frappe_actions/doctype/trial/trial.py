ffa9d2425dd5a55dde011a7d6d189811b8841568
Mitul Garg <mitulgarg3@gmail.com>
2024-03-21 13:04:30 +0530

    Frappe UI app

---
+++ frappe_actions/frappe_actions/doctype/trial/trial.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Mitul Garg and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Trial(Document):
+	pass
